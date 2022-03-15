a0a96c97f2887f315ef913287a0cdcd1baa292a6
Eric Vogler <ericvogler@gmail.com>
2022-03-14 22:25:58 -0700

    move to single symbol layer and text-editing layer

--- keyboards/spaceman/pancake/rev2/keymaps/pancake_ev1/keymap.c
+++ keyboards/spaceman/pancake/rev2/keymaps/pancake_ev1/keymap.c
@@ -64,7 +64,6 @@ enum combo_events {
   TABLEFT_COMBO,
   TABRIGHT_COMBO,
   SWAPHANDS_COMBO,
-  CUSTOM_COMMA,
   COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
@@ -124,7 +123,6 @@ const uint16_t PROGMEM tabright_combo[] = {KC_N, KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM optbksp_combo[] = {KC_COMMA, KC_I, KC_O,COMBO_END};
 const uint16_t PROGMEM guibksp_combo[] = {KC_K, KC_N, KC_E, COMBO_END};
 const uint16_t PROGMEM swaphands_combo[] = {KC_SPC, MO(2), COMBO_END};
-const uint16_t PROGMEM custom_comma[] = {KC_COMMA, COMBO_END};
 
 combo_t key_combos[] = {
   [TAB_COMBO] = COMBO_ACTION(tab_combo),
@@ -173,7 +171,6 @@ combo_t key_combos[] = {
   [TABLEFT_COMBO] = COMBO_ACTION(tableft_combo),
   [TABRIGHT_COMBO] = COMBO_ACTION(tabright_combo),
   [SWAPHANDS_COMBO] = COMBO_ACTION(swaphands_combo),
-  [CUSTOM_COMMA] = COMBO_ACTION(custom_comma)
 
 };
 /* COMBO_ACTION(x) is same as COMBO(x, KC_NO) */
@@ -201,8 +198,6 @@ combo_t key_combos[] = {
 //     tap_code16(num_to_code((num >> 0) & 0x0F));
 //     tap_code16(KC_SPC);
 // }
-
-uint8_t custom_comma_shifted = false;
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
   switch(combo_index) {
@@ -537,23 +532,6 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
             unregister_code16(KC_RBRC);
         }
         break;
-    case CUSTOM_COMMA:
-        if (pressed) {
-            if (get_mods() & MOD_MASK_SHIFT) {
-                register_code(KC_E);
-                custom_comma_shifted = true;
-            } else {
-                register_code(KC_COMM);
-                custom_comma_shifted = false;
-            }
-        } else {
-            if (custom_comma_shifted) {
-                unregister_code(KC_E);
-            } else {
-                unregister_code(KC_COMM);
-            }
-        }
-        break;
     }
 }
 
@@ -725,13 +703,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_W, KC_F, KC_P, KC_B,  _, _,  KC_J, KC_L, KC_U, KC_Y, KC_LEAD,
         KC_A, KC_R, KC_S, KC_T, KC_G,  _, _,  KC_M, KC_N, KC_E, KC_I, KC_O,
         LSFT_T(KC_Z), KC_X, KC_C, KC_D, KC_V,  _, _,  KC_K, KC_H, KC_COMM, KC_DOT, RSFT_T(KC_QUOT),
-        _, _, _,  MO(1), TD(ALT_LP),  _, _,  KC_SPC, MO(2), _, _, _
+        _, _, _,  MO(1), TD(ALT_LP),    _, _,   KC_SPC, MO(4), _, _, _
         ),
 	[1] = LAYOUT_ortho_4x12(
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,  _, _,  KC_CIRC, KC_AMPR, KC_ASTR, _, _,
+        KC_LT, KC_LCBR, KC_LBRC, KC_LPRN, KC_SLSH,  _, _,  KC_GRV, KC_SCLN, KC_EQL, KC_MINS, KC_BSLS,
         KC_1, KC_2, KC_3, KC_4, KC_5,              _, _,  KC_6, KC_7, KC_8, KC_9, KC_0,
-        OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), _,   _, _,    KC_BSPC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-        _, _, _, KC_TRNS, _,    _, _,   _, MO(3), _, _, _
+        KC_LSFT, _, _, _, _,   _, _,     _, _, _, _, KC_RSFT,
+        _, _, _, KC_TRNS, _,    _, _,   KC_SPC, MO(3), _, _, _
         ),
 	[2] = LAYOUT_ortho_4x12(
         KC_GT, KC_RCBR, KC_RBRC, KC_RPRN, _,    _, _,    KC_GRV, KC_MINS, KC_PPLS, KC_TILDE, _,
@@ -746,9 +724,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _, _, _, KC_TRNS, _,    _, _,    _, KC_TRNS, _, _, _
         ),
     [4] = LAYOUT_ortho_4x12(
-        X(BANG), X(BANG), X(BANG), X(BANG), X(BANG), _, _, KC_B, KC_P, KC_F, KC_W, KC_Q,
-        X(BANG), X(BANG), X(BANG), X(BANG), X(BANG), _, _, KC_G, KC_T, KC_S, KC_R, KC_A,
-        X(BANG), X(BANG), X(BANG), X(BANG), X(BANG), _, _, KC_V, KC_D, KC_C, KC_X, KC_Z,
+        X(BANG), G(KC_W), X(BANG), X(BANG), X(BANG), _, _, KC_PGUP, KC_TAB, KC_BSPC, KC_HOME, KC_END,
+        G(KC_A), KC_LSHIFT, KC_LALT, KC_LGUI, X(BANG), _, _, KC_PGDN, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
+        G(KC_Z), G(KC_X), G(KC_C), G(KC_D), G(KC_V), _, _, KC_ENT, A(KC_LEFT), A(KC_BSPC), _, A(KC_RIGHT),
         X(BANG), X(BANG), X(BANG), X(BANG), X(BANG), _, _, KC_TRNS, KC_TRNS, _, _, _
     )
 };
@@ -774,6 +752,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         last_keycode = KC_NO;
     }
 
+    uint8_t override = false;
+
     if (record->event.pressed) {
         switch (keycode) {
             case FORCE_REP:
@@ -783,7 +763,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 clear_mods();
                 layer_move(0);
                 break;
-
+            case KC_DOT:
+                if (get_mods() & (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))) {
+                    tap_code16(KC_P);
+                } else {
+                    tap_code16(KC_DOT);
+                }
+                override = true;
+                break;
+            case KC_LT:
+                if (get_mods() & (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))) {
+                    tap_code16(KC_GT);
+                    override = true;
+                }
+                break;
+            case KC_LPRN:
+                if (get_mods() & (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))) {
+                    tap_code16(KC_RPRN);
+                    override = true;
+                }
+                break;
+            case KC_LBRC:
+                if (get_mods() & (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))) {
+                    tap_code16(KC_RBRC);
+                    override = true;
+                }
+                break;
+            case KC_LCBR:
+                if (get_mods() & (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))) {
+                    tap_code16(KC_RCBR);
+                    override = true;
+                }
+                break;
         }
     }
 
@@ -792,6 +803,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // only a single modifier from the previous key is repeated (e.g. Ctrl+Shift+T then Repeat produces Shift+T)
     mod_state = get_mods();
     oneshot_mod_state = get_oneshot_mods();
+    if (override) {
+        return false;
+    }
     return true;
 };
 
